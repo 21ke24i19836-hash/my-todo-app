1bbe0cb1312ade6c09648fef0192c0ea70075ca0
anilr <anilroche12@gmail.com>
2026-05-25 21:44:11 +0530

    Creat basic GUI without interactions, day 25-05-2026

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import time
+
+
 def get_todos(filepath="../../ToDo list saved items/todos.txt"):
     with open(filepath, "r") as file:
         todos_local = file.readlines()
     return todos_local
 
-
+now = time.strftime("%Y-%m-%d %H:%M:%S")
+print("it is", now)
 def write_todos(todos_arg, filepath="../../ToDo list saved items/todos.txt"):
     with open(filepath, "w") as file:
         file.writelines(todos_arg)
